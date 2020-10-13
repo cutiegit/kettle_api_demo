b79875e3605f46fd2440571c0093b1bc2b662412
root <tyzh0205@163.com>
2020-10-13 16:25:38 +0800

    初始化kettle api 项目

---
+++ src/main/java/cn/para/kettle_api_demo/KettleApiDemoApplication.java
@@ -0,0 +1,13 @@
+package cn.para.kettle_api_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KettleApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KettleApiDemoApplication.class, args);
+	}
+
+}
